c308cadcf3504bd53e57a8ca7e6772d3b1ad6946
Guillaume-It <guillaume-github@outlook.fr>
2021-01-31 12:19:11 +0100

    add a thread to manage the servo

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,48 +1,56 @@
 #include "hardwareSerial.h"
 #include <stdio.h>
 #include <Servo.h>
+#include <pt.h>   // include protothread library
+static struct pt ptServo, ptMotor; // each protothread needs one of these
 
-
+//Message
 const static String BLUETOOTH_INITIALISATION = "I";
 const static String BLUETOOTH_CONNECTED = "C";
 const static String BLUETOOTH_SERVO = "S";
 const static String BLUETOOTH_MOTOR = "M";
-
-#define PIN_Servo 3
-Servo servo;             //  Create a DC motor drive object
-unsigned int carSpeed_rocker = 250;
-#define ENA 5
-#define ENB 6
-#define IN1 7
-#define IN2 8
-#define IN3 9
-#define IN4 11
-
-#define LED_Pin 13
-
 const static byte idSeparator = 2;
+
+//Servo
+const static char PIN_Servo = 3;
+static uint8_t angleSettingOld;
+static uint8_t angleSetting = 90;
+Servo servo;             //  Create a DC motor drive object
+
+//Moto
+unsigned int carSpeed_rocker = 250;
+const static char ENA = 5;
+const static char ENB = 6;
+const static char IN1 = 7;
+const static char IN2 = 8;
+const static char IN3 = 9;
+const static char IN4 = 11;
+
+const static char LED_Pin = 13;
+
 
 void setup() {
   //Bluetooth
   Serial.begin(9600);
-  ServoControl(90);
+  servo.attach(3);
 
   //Motor
-  //  pinMode(IN1, OUTPUT); //Motor-driven port configuration
-  //  pinMode(IN2, OUTPUT);
-  //  pinMode(IN3, OUTPUT);
-  //  pinMode(IN4, OUTPUT);
-  //  pinMode(ENA, OUTPUT);
-  //  pinMode(ENB, OUTPUT);
-  //  digitalWrite(ENA, HIGH);  //Enable left motor
-  //  digitalWrite(ENB, HIGH);  //Enable right motor
-  pinMode(IN3, OUTPUT); //set IO pin mode OUTPUT
+  pinMode(IN1, OUTPUT); //Motor-driven port configuration
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
+  digitalWrite(ENA, HIGH);  //Enable left motor
   digitalWrite(ENB, HIGH);  //Enable right motor
+
+  //Thread
+  PT_INIT(&ptServo);  // initialise the two
+  PT_INIT(&ptMotor);  // protothread variables
 }
 
 void loop() {
+  servoControl(&ptServo, 100);
   while ( Serial.available() > 0) //Forcibly wait for a frame of data to finish receiving
   {
     String    c =   Serial.readString();
@@ -67,7 +75,7 @@ void manageMessage(String message, int end)
     Serial.println("{" + BLUETOOTH_CONNECTED + ":}");
     Serial.flush();
   } else if (BLUETOOTH_SERVO == key) {
-    ServoControl(value.toInt());
+    angleSetting = value.toInt();
   } else if (BLUETOOTH_MOTOR == key) {
     int motor = value.toInt();
     if (motor >= 0) {
@@ -79,23 +87,38 @@ void manageMessage(String message, int end)
   }
 }
 
+void servoControl(uint8_t angle){
+      if (angleSettingOld != angle) {
+      angleSettingOld = angle;
+      if (angle > 175)
+      {
+        angle = 175;
+      }
+      else if (angle < 5)
+      {
+        angle = 5;
+      }
+      servo.write(angle); //sets the servo position according to the  value
+    }
+}
+
+
 /*
   Servo Control angle Setting
 */
-void ServoControl(uint8_t angleSetting)
+static int servoControl(struct pt *pt, int interval)
 {
-  if (angleSetting > 175)
-  {
-    angleSetting = 175;
+  static unsigned long timestamp = 0;
+  PT_BEGIN(pt);
+  while (1) { // never stop
+    /* each time the function is called the second boolean
+       argument "millis() - timestamp > interval" is re-evaluated
+       and if false the function exits after that. */
+    PT_WAIT_UNTIL(pt, millis() - timestamp > interval);
+    timestamp = millis(); // take a new timestamp
+    servoControl(angleSetting);
   }
-  else if (angleSetting < 5)
-  {
-    angleSetting = 5;
-  }
-  servo.attach(3);
-  servo.write(angleSetting); //sets the servo position according to the  value
-  delay(500);
-  servo.detach();
+  PT_END(pt);
 }
 
 /*
